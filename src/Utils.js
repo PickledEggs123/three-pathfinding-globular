a0125524db086a04b9e3b1f7361c78abf5e99622
Tyler Truong <tyler_truong@icloud.com>
2025-11-27 00:11:39 -0500

    Updated isPointInPoly because the previous version might be broken. It was not broke, revert.

--- src/Utils.js
+++ src/Utils.js
@@ -24,11 +24,15 @@ class Utils {
   //+ Jonas Raoni Soares Silva
   //@ http://jsfromhell.com/math/is-point-in-poly [rev. #0]
   static isPointInPoly (poly, pt) {
-      for(var c = false, i = -1, l = poly.length, j = l - 1; ++i < l; j = i)
-          ((poly[i].y <= pt.y && pt.y < poly[j].y) || (poly[j].y <= pt.y && pt.y < poly[i].y))
-          && (pt.x < (poly[j].x - poly[i].x) * (pt.y - poly[i].y) / (poly[j].y - poly[i].y) + poly[i].x)
-          && (c = !c);
-      return c;
+      const dotArray = [];
+      for (let i = 0; i < poly.length; i++) {
+          const a = poly[i];
+          const b = poly[(i + 1) % poly.length];
+          const cross = new THREE.Vector3().crossVectors(a, b);
+          const dot = cross.clone().dot(pt.clone());
+          dotArray.push(dot);
+      }
+      return dotArray.every(dot => dot >= 0) || dotArray.every(dot => dot <= 0);
   }
 
   static isVectorInPolygon (vector, polygon, vertices) {
